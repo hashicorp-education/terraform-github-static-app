f2468599d8ec18e409ff99fd0c53f6e60117a966
Anthony <russo555@gmail.com>
2023-08-28 14:42:20 -0400

    Updated template and destination orgs.

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "template_org" {
   description = "The name of the organization in Github that will contain the example app repo."
-  default     = "hashicorp-education"
+  default     = "tunzor"
 }
 
 variable "template_repo" {
@@ -10,7 +10,7 @@ variable "template_repo" {
 
 variable "destination_org" {
   description = "The name of the organization in Github that will contain the templated repo."
-  default     = "tunzor"
+  default     = "hashicorp-education"
 }
 
 variable "gh_token" {
