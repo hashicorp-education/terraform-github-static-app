bc23edd8427f3dbf1d8e8cf553b1f92e03274af3
Jamie White <jamie@jgwhite.co.uk>
2024-09-23 14:44:32 +0200

    Add `repo_full_name` output var
    Useful for tutorials

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "repo_url" {
   description = "The URL of the created repository."
 }
 
+output "repo_full_name" {
+  value       = github_repository.gh_repo.full_name
+  description = "The full name (including org) of the created repository."
+}
+
 output "app_url" {
   value       = "https://${var.destination_org}.github.io/${var.waypoint_application}"
   description = "The URL of the app on GitHub Pages."
